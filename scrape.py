a887c881fbe9996bfbbe2449b2350b0c3e1b031c
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-02 18:06:40 +0900

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,24 +4,23 @@ import json
 import requests
 
 # アクセス先URLとUAを設定
-url = 'https://news.yahoo.co.jp/topics'
-ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '\
-    'AppleWebKit/537.36 (KHTML, like Gecko) '\
-    'Chrome/77.0.3865.90 Safari/537.36 '
+url = 'https://sports.yahoo.co.jp/news/list?id=ws'
+ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) ' \
+     'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+     'Chrome/77.0.3865.90 Safari/537.36 '
+
 
 def getNews(word):
-
     # 下記はアクセス先に合わせて修正しスクレイピング
     req = urllib.request.Request(url, headers={'User-Agent': ua})
     html = urllib.request.urlopen(req)
     soup = BeautifulSoup(html, "html.parser")
-    main = soup.find('div', attrs={'class': 'topicsMod'})
-    topics = main.select("li > a")
+    main = soup.find('div', attrs={'class': 'modBody'})
+    topics = main.find_all(attrs={'class': 'linkMain'})
 
     # 該当記事カウント変数と結果格納リスト
     count = 0
     list = []
-
 
     # スクレイピング結果から引数wordを含む記事を結果リストに格納
     for topic in topics:
